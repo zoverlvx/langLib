1d568123b59afc38661d63a6b948e3f4dc74921d
zoverlvx <zoverhulser@gmail.com>
2017-11-13 23:20:16 -0500

    Testing LanguageBtn component to generate path /french on click

--- src/test/routes.test.js
+++ src/test/routes.test.js
@@ -43,25 +43,52 @@ describe("Routes", () => {
     it.only("has a path of /", () => {
         const component = wrapper.find("Route").first();  
         const prop = component.props().path;
-        console.log("Here is wrapper: ", wrapper);
-        console.log("Here is component: ", component);
-        console.log("Here is the prop which should be path: ", prop);
-        console.log("Here is component.props", component.props());
         expect(prop).toEqual("/");      
     });
 
-     it.only("generates a path of /french", () => {
-        let wrapper = mount(<Router><Provider store={store}><Routes /></Provider></Router>);
-        // let w = wrapper.find(LanguageBtn).first().props().name;
-        // let w = wrapper.find(LanguageBtn).first().props();
-        let w = wrapper.find(LanguageBtn).first().debug();
-        let component = wrapper.find(LanguageBtn).first().simulate("click");
-        console.log("Debug of LanguageBtn", w);
-        console.log("LanguageBtn", component);  
-        expect(component).toEqual("/french");
-
+    it.only("generates a path of /french", () => {
+    
     });
+/*
+     it.only("generates a path of /french", () => {
+       const wrapper = mount(<Router><Provider store={store}><Routes /></Provider></Router>);
+        const propsOfLb = wrapper.find(LanguageBtn).first().props();
+        const lbPropName = wrapper.find(LanguageBtn).first().props().name;
+        const debugOfLb = wrapper.find(LanguageBtn).first().debug();
+        const propsOfLink = wrapper.find(LanguageBtn).first().find("Link").props();
+        const linkPropTo = wrapper.find(LanguageBtn).first().find("Link").props().to;
+        const propsOfA = wrapper.find(LanguageBtn).first().find("a").props();
+        const aPropHref = wrapper.find(LanguageBtn).first().find("a").props().href;
+        const clickedLink = wrapper.find(LanguageBtn).first().find("Link").first().simulate("click").props().to;
+        const clickedA = wrapper.find(LanguageBtn).first().find("a").first().simulate("click");
+        const propsOfRoute = wrapper.find(LanguageBtn).first().find("Route").props();
+        // console.log("Props of Link: ", propsOfLink);        
+        // console.log("Prop To of Link: ", linkPropTo);
+        // console.log("Props of A: ", propsOfA);
+        // console.log("Prop Href of A: ", aPropHref);
+        // console.log("clickedLink: ", clickedLink);
+        // console.log("clickedA: ", clickedA);  
+        const firstRoute = wrapper.find("Routes").find("Route").at(0).debug();
+        const dbOfConnectedRoute = wrapper.find("Routes").find("Route").find("Connect").first().debug();
+        const propsOfConnectedRoute = wrapper.find("Routes").find("Route").find("Connect").first().props();
+        const lb = wrapper.find("Routes").find("Route").find("Connect").find("LanguageBtn").first().props().location;
+        const clickedLb = wrapper.find("Routes").find("Route").find("Connect").find("LanguageBtn").first().simulate("click").props().location;
+        console.log("HERE IS PROP LOCATION OF LB: ", lb);
+        console.log("HERE IS PROP LOCATION OF LB AFTER CLICK: ", clickedLb);
+        const propLocation = wrapper.find("Routes").find("Route").find("Connect").first().props().location;
+        const clickedLocation = wrapper.find("Routes").find("Route").find("Connect").first().simulate("click").props().location;
+        //console.log("Here's the first route: ", firstRoute);
+        console.log("Debug of connected LanguageBtn: ", dbOfConnectedRoute);
+        console.log("Props of connected Route: ", propsOfConnectedRoute);
+        console.log("Should be prop location: ", propLocation);
+        console.log("Prop location after click: ", clickedLocation);
 
+
+        expect(linkPropTo).toEqual("/french");
+        expect(aPropHref).toEqual("/french");
+        expect(clickedLink).toEqual("/french");
+    });
+*/
     it.only("renders Default component on /", () => {
         console.log("Here is wrapper.debug", wrapper.debug());
     });
