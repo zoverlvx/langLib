d9710babe35f8083a7863d83e601424276072664
zoverlvx <zoverhulser@gmail.com>
2017-12-09 19:38:40 -0500

    Removing comments from index.js and files in dir reducers

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,9 +1,7 @@
 import {combineReducers} from "redux";
-//import {routeReducer} from "react-router-redux";
 import languageReducer from "./language-reducer";
 import dropdown from "./dropdown";
 
-//reducers gathered into one
 const allReducers = combineReducers({
 	languages: languageReducer,
 	dropdown: dropdown
